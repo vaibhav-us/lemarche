2bea230f41c4324cbef0a47c8f2a345ae60be8bb
vaibhav-us <vaibhavkrishnavus@gmail.com>
2024-07-08 18:04:17 +0530

    poss stable1

--- core/routing.py
+++ core/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<user1>[^/]+)/(?P<user2>[^/]+)/$', consumers.ChatConsumer.as_asgi()),
+    re_path(r'ws/chat/(?P<user1>[^/]+)/(?P<user2>[^/]+)/(?P<product>[^/]+)/$', consumers.ChatConsumer.as_asgi()),
 ]
