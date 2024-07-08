51e200744f0ba05b883b65f2a6825d812795aa42
vaibhav-us <vaibhavkrishnavus@gmail.com>
2024-07-08 15:07:10 +0530

    12456

--- lemarche/settings.py
+++ lemarche/settings.py
@@ -93,7 +93,7 @@ CHANNEL_LAYERS = {
     'default':  {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [os.environ.get['REDIS_URL']],
+            "hosts": [os.environ.get('REDIS_URL')],
         },
     },
 }
